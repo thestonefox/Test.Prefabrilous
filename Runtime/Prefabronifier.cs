a5949db88866f88fcfc721a3157006186502e78e
Harvey Ball <me@harveyball.com>
2019-10-09 10:06:21 +0100

    first commit

---
+++ Runtime/Prefabronifier.cs
@@ -0,0 +1,13 @@
+﻿namespace TheStonefox.Test.Prefabrilous
+{
+    using TheStonefox.Test.Zinpipa;
+
+    public class Prefabronifier
+    {
+        public virtual string Prefabrone()
+        {
+            Zinpipifier z = new Zinpipifier();
+            return "[PREFABRONE] " + z.Zinpipify();
+        }
+    }
+}
---
+++ Runtime/Prefabs/Debogz/SharedResources/Deboginator.cs
@@ -0,0 +1,14 @@
+﻿namespace TheStonefox.Test.Prefabrilous.Prefabs.Debogz
+{
+    using UnityEngine;
+
+    public class Deboginator : MonoBehaviour
+    {
+        Prefabronifier p = new Prefabronifier();
+
+        protected virtual void OnEnable()
+        {
+            Debug.Log(p.Prefabrone());
+        }
+    }
+}
